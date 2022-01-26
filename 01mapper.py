837b26f9d4cc6ea65469eafe0169212c89472635
dipikaxsharma <s530527@nwmissouri.edu>
2022-01-26 14:44:59 -0600

    01 mapper

---
+++ 01mapper.py
@@ -0,0 +1,9 @@
+#dipika Sharma
+#practice mapper
+f = open("purchases.txt", "r")
+o = open("a.text","w")
+for line in f:
+   rowList = line.strip(),split("    ")
+   print(rowlist )
+   print (len(rowlist ))
+   
