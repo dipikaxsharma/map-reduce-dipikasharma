2d00c381e2b5ee8c69fa9da4eac4de8ec3cdaaec
dipikaxsharma <s530527@nwmissouri.edu>
2022-01-31 10:48:47 -0600

    mapper, reduce, sort

--- 01mapper.py
+++ 01mapper.py
@@ -1,9 +1,17 @@
 #dipika Sharma
 #practice mapper
+from posixpath import split
+
+
 f = open("purchases.txt", "r")
 o = open("a.text","w")
 for line in f:
    rowList = line.strip(),split("    ")
-   print(rowlist )
-   print (len(rowlist ))
-   
+   print(rowList )
+   print (len(rowList ))
+   if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
---
+++ sorter.py
@@ -0,0 +1,11 @@
+#Name: Dipika Sharma
+n = open("o.txt","r")  # open file, read-only
+s = open("s.txt", "w") # open file, write
+lines = n.readlines()
+lines.sort()
+
+for line in lines:
+ s.write(line)
+
+n.close()
+s.close()
