aa37e0ca266c0b1a2c92b70a0f357d8d4b0adf74
dipikaxsharma <s530527@nwmissouri.edu>
2022-02-09 11:55:45 -0600

    ,,

--- 13reducer.py
+++ 13reducer.py
@@ -6,19 +6,19 @@ thisValue = 0.0
 for line in sys.stdin:
   datalist = line.strip().split('\t')
   if (len(datalist) == 2) : 
-    name, cook_time = datalist
+    foodName, count = datalist
 
-    if name != thisKey:   # we've moved to another key
+    if foodName != thisKey:   # we've moved to another key
       if thisKey:
         # output the previous key-summaryvalue result
         print(thisKey,'\t',thisValue)
 
       # start over for each new key
-      thisKey = name 
+      thisKey = foodName 
       thisValue = 0.0
   
     # apply the aggregation function
-    thisValue += float(cook_time)
+    thisValue += float(count)
 
 # output the final key-summaryvalue result outside the loop
 print(thisKey,'\t',thisValue)
